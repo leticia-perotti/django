69b96865f0c9100c6a3d7156a31457d07d67f457
Leticia Daniele Perotti <80782049+leticia-perotti@users.noreply.github.com>
2022-03-30 18:04:34 -0300

    estilo

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,6 +3,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
+
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('overall_score/', views.overall_score, name='overall_score')
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,18 +1,37 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Question
+import re
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import Choice, Question
 # Create your views here.
 
 def index(request):
-    latest_question_list = Question.objects.order_by('pub_date')[:5]
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
-
-def detail(request, question_id):
-    return HttpResponse("Essa é a pergunta de número %s" % question_id)
+    return render(request,'polls/index.html', context)
 
 def results(request, question_id):
-    return HttpResponse("Esses são os resultados da pergunta de número %s" % question_id)
+    question = Question(pk=question_id)
+    return render(request, 'polls/results.html', {'question' : question})
+
+def overall_score(request):
+    question_all = Question.objects.all()
+
+    question_id = Choice.objects.filter(question_id= id)
+    context = {'question_all': question_all, 'question_id' : question_id}
+    return render(request, 'polls/overall_score.html', context)
 
 def vote(request, question_id):
-    return HttpResponse("Você está votando na questão de número %s" %question_id)
+    question = get_object_or_404(Question, pk = question_id)
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except KeyError:
+        return render(request, 'polls/vote.html', {
+            'question': question,
+            'error_message': "Você não escolheu uma pergunta!!"
+        })
+    else:
+        selected_choice.vote += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
